e1eaf0f19870c2b361c7db36635a1142694cbaaf
sharayura <sharapyura@gmail.com>
2022-08-01 22:41:00 +0300

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double bananWeight = 80;
+        double bananNum = 5;
+        double milk100 = 105;
+        double milkNum = 200;
+        double icecreamWeight = 100;
+        double icecreamNum = 2;
+        double eggWeight = 70;
+        double eggNum = 4;
+        double allWeight = bananWeight * bananNum + milk100/100 * milkNum + icecreamWeight * icecreamNum + eggWeight * eggNum;
+        double allWeightKg = allWeight / 1000;
+        System.out.println("Вес спорт-завтрака " + allWeightKg + " кг." );
     }
 }
