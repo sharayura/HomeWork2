331e58c23f95814a89ea0ffaec43526db49e79c9
sharayura <sharapyura@gmail.com>
2022-08-01 23:44:22 +0300

    инишиал

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        int minusWeight = 7000;
-        int minusDayMin = 250;
-        int minusDayMax = 500;
-        int daysMax = minusWeight / minusDayMin;
-        int daysMin = minusWeight / minusDayMax;
-        int daysAv = (daysMin + daysMax)/2;
-        System.out.println("Требуется от " + daysMin + " до " + daysMax + " дней, в среднем " +
-                daysAv + " дней.");
+        System.out.println("Hello world!");
     }
 }
